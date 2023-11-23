1ed42993265c053192a9a8cd8e074401303d7476
longnt <nice231096@gmail.com>
2023-11-23 22:53:27 +0700

    add meal

---
+++ CateringManagement/Models/DTO/MealDetailDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace CateringManagement.Models.DTO
+{
+    public class MealDetailDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<MealIngredientDTO> Ingredients { get; set; }
+    }
+
+    public class MealIngredientDTO
+    {
+        public Guid IngredientId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
