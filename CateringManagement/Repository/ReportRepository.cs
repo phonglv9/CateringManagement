567dac71c4fe9da5c4915114ba0fb39fb70c0317
phonglv <lovanphong2299@gmail.com>
2023-11-23 20:24:58 +0700

    ok

--- CateringManagement/Repository/ReportRepository.cs
+++ CateringManagement/Repository/ReportRepository.cs
@@ -9,35 +9,45 @@ namespace CateringManagement.Repository
     {
         public async Task <List<ReportDTO>> GetDataReport(DateTime dateStart, DateTime dateEnd)
         {
-            var reports = new List<ReportDTO>();
-
-            // Tạo danh sách các ngày cần thống kê
-             var dates = Enumerable.Range(0, (dateEnd - dateStart).Days + 1)
-                                  .Select(i => dateStart.AddDays(i))
-                                  .ToList();
-
-            //Duyệt qua từng ngày và tính toán doanh thu, tổng đơn hàng
-            foreach (var date in dates)
+            try
             {
-                var revenue =   db.Orders
-                            .Where(od => od.CreatedAt.Date == date.Date && od.Status == OrderStatus.Done && od.IsDeleted != 1)
-                .Sum(od => od.TotalPrice);
+                var reports = new List<ReportDTO>();
 
-                var totalOrders =  db.Orders
-                                    .Where(o => o.CreatedAt.Date == date.Date && o.Status == OrderStatus.Done && o.IsDeleted != 1)
-                                    .Count();
+                // Tạo danh sách các ngày cần thống kê
+                var dates = Enumerable.Range(0, (dateEnd - dateStart).Days + 1)
+                                     .Select(i => dateStart.AddDays(i))
+                                     .ToList();
 
-                ReportDTO reportDto = new ReportDTO
+                //Duyệt qua từng ngày và tính toán doanh thu, tổng đơn hàng
+                foreach (var date in dates)
                 {
-                    DateValue = date,
-                    Order = totalOrders,
-                    TotalMoney = revenue
-                };
+                    var revenue = db.Orders
+                                .Where(od => od.CreatedAt.Date == date.Date && od.Status == OrderStatus.Done && od.IsDeleted != 1)
+                    .Sum(od => od.TotalPrice);
 
-                reports.Add(reportDto);
+
+
+                    var totalOrders = db.Orders
+                                        .Where(o => o.CreatedAt.Date == date.Date && o.Status == OrderStatus.Done && o.IsDeleted != 1)
+                                        .Count();
+
+                    ReportDTO reportDto = new ReportDTO
+                    {
+                        DateValue = date,
+                        Order = totalOrders,
+                        TotalMoney = revenue
+                    };
+
+                    reports.Add(reportDto);
+                }
+                return reports;
             }
+            catch (Exception ex)
+            {
 
-            return  reports;
+                throw;
+            }
+           
         }
     }
 }
